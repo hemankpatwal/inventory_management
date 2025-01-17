49139d990a8f8a368d6b171123f9136334ad4382
hemankpatwal <98538477+hemankpatwal@users.noreply.github.com>
2025-01-18 02:54:15 +0530

    core/templates/core base.html add_product.html add_supplier.html product_list.html added

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('products/', views.list_products, name='product_list'),
+    path('product/add/', views.add_product, name='add_product'),
+    path('suppliers/', views.list_suppliers, name='supplier_list'),
+    path('supplier/add/', views.add_supplier, name='add_supplier'),
+    path('stock_movement/add/', views.add_stock_movement, name='add_stock_movement'),
+    path('sale_order/create/', views.create_sale_order, name='create_sale_order'),
+]
